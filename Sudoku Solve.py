5028fe53f09d86fbaf030b02a86326eb87c1d1bc
A Sreeram <asreeram8a@gmail.com>
2025-11-09 08:15:38 +0530

    Sudoku Solve.py

--- Sudoku Solve.py
+++ Sudoku Solve.py
@@ -1,3 +1,7 @@
+print("WELCOME TO PYTHON PROGRAMMING LANGUAGE")
+print()
+print("TO SOLVE A GIVEN 9X9 SUDOKU")
+print()
 # Sudoku Solver using Backtracking
 
 def print_board(board):
@@ -64,15 +68,15 @@ def solve(board):
 
 # Example Sudoku puzzle (0 = empty cell)
 sudoku_board = [
-    [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    [0, 0, 0, 0, 0, 4, 0, 0, 0],
+    [0, 4, 0, 0, 8, 6, 0, 0, 0],
+    [0, 0, 0, 7, 0, 0, 0, 3, 2],
+    [0, 0, 6, 0, 9, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 0, 0, 5, 3],
+    [0, 0, 0, 5, 3, 0, 6, 2, 0],
+    [8, 0, 0, 0, 0, 0, 0, 0, 7],
+    [0, 0, 5, 4, 2, 0, 0, 6, 0],
+    [0, 0, 1, 8, 7, 0, 0, 0, 5]
 ]
 
 print("Original Sudoku:")
